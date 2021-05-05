cc3167692958ddcf14cfee082c53812d0ff9e971
Łukasz Wyrostek <fayth77777@gmail.com>
2021-05-05 12:15:57 +0200

    fill grid with tile types of density v.1

--- SDL_nauka/Include/TileMap.h
+++ SDL_nauka/Include/TileMap.h
@@ -8,8 +8,10 @@
 struct TileGrid
 {
 public: 
-	TileGrid() : tiles_single_grid(30, std::vector < Tile*>(30)) {}
+	TileGrid() : tiles_single_grid(0, std::vector < Tile*>(0)), tiles_amount(0){};
+	TileGrid(int xsize, int ysize) : tiles_single_grid(xsize, std::vector < Tile*>(ysize)),tiles_amount(xsize*ysize) {}
 	std::vector<std::vector<Tile*>> tiles_single_grid;
+	int tiles_amount;
 };
 
 class TileMap
@@ -31,7 +33,7 @@ public:
 	TileGrid FillSingleGrid(int gridx, int gridy);
 	void SetAllTileType();
 	void SetTileTexture();
-	void SetGridType(TileGrid &grid, enum class TileType type);
+	void SetGridType(TileGrid &grid,int density, enum class TileType type1, enum class TileType type2);
 
 	inline int GetGridsVectorSideSize() { return grids_vector_side_size; };
 	inline int GetGridRowSize() { return grid_row_size; };
@@ -46,8 +48,8 @@ private:
 	
 	const int kTile_size = 32;
 	Tile starting_tile;
-	inline static int column_total_amount = 300;
-	inline static int row_total_amount = 300;
+	inline static int column_total_amount = 100;
+	inline static int row_total_amount = 100;
 	int grids_vector_side_size = 10;
 	int grid_row_size = row_total_amount / grids_vector_side_size;
 	int grid_column_size = column_total_amount / grids_vector_side_size;
--- SDL_nauka/src/TileMap.cpp
+++ SDL_nauka/src/TileMap.cpp
@@ -29,7 +29,7 @@ void TileMap::FillAllTilesGrids()
 
 TileGrid TileMap::FillSingleGrid(int gridx, int gridy)
 {
-	TileGrid tiles_grid;
+	TileGrid tiles_grid(TileMap::GetTileMap().GetGridRowSize(), TileMap::GetTileMap().GetGridColumnSize());
 	
 	for (size_t x = 0; x < grid_row_size; x++)
 	{
@@ -51,24 +51,68 @@ void TileMap::SetAllTileType()
 		{
 			for (size_t gridy = 0; gridy < grids_vector_side_size; gridy++)
 			{
-				SetGridType(tiles_grids[gridx][gridy], TileType::TILETYPE_GRASS);
+				if (gridx < 2)
+				{
+					SetGridType(tiles_grids[gridx][gridy],67, TileType::TILETYPE_GRASS, TileType::TILETYPE_GROUND);
+				}
+				if (gridx == 2 )
+				{
+					SetGridType(tiles_grids[gridx][gridy],78, TileType::TILETYPE_GROUND, TileType::TILETYPE_FOREST);
+				}
+				if (gridx > 2 )
+				{
+					SetGridType(tiles_grids[gridx][gridy],35, TileType::TILETYPE_FOREST, TileType::TILETYPE_GROUND);
+				}
 			}
 		}
 	}
 }
 
-void TileMap::SetGridType(TileGrid& grid, enum class TileType type)
+void TileMap::SetGridType(TileGrid& grid, int density, enum class TileType type1, enum class TileType type2)
 {
+	int type1_tile_amount = floor(grid.tiles_amount * (density / 100.0));
+	int type2_tile_amount = floor(grid.tiles_amount - type1_tile_amount);
+	int x = 0; // current position; x
+	int y = 0; // current position; y
+	int d = 0; // current direction; 0=RIGHT, 1=DOWN, 2=LEFT, 3=UP
+	int c = 0; // counter
+	int s = 1; // chain size
 
-	for (size_t x = 0; x < grid_row_size; x++)
+	// starting point
+	x = ((int)floor(grids_vector_side_size / 2.0))-1;
+	y = ((int)floor(grids_vector_side_size / 2.0))-1;
+
+	for (int k = 1; k <= (grids_vector_side_size - 1); k++)
 	{
-		for (size_t y = 0; y < grid_column_size; y++)
+		for (int j = 0; j < (k < (grids_vector_side_size - 1) ? 2 : 3); j++)
 		{
+			for (int i = 0; i < s; i++)
+			{
+				if (type1_tile_amount>0)
+				{
+					grid.tiles_single_grid[x][y]->SetTileType(type1);
+					type1_tile_amount--;
+				}
+				else
+				{
+					grid.tiles_single_grid[x][y]->SetTileType(type2);
 
-			grid.tiles_single_grid[x][y]->SetTileType(type);
+				}
+
+				c++;
+				switch (d)
+				{
+				case 0: y = y + 1; break;
+				case 1: x = x + 1; break;
+				case 2: y = y - 1; break;
+				case 3: x = x - 1; break;
+				}
+			}
+			d = (d + 1) % 4;
 		}
+		s = s + 1;
 	}
-
+	
 }
 
 
